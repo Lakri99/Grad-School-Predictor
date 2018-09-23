f8288093767cc7bbbe1fe4cb624575c47ca80465
Lakri99 <rblakshmi3@gmail.com>
2018-09-23 22:40:58 +0530

    column seperation 50%

--- Run.py
+++
@@ -1,6 +0,0 @@
-import universityExtractor as U_ex
-import studentExtractor as S_ex
-
-S_ex.generateStudentList(440)
-
-# test
--- test.py
+++
@@ -1,6 +0,0 @@
-import universityExtractor as U_ex
-import studentExtractor as S_ex
-
-url = "https://yocket.in/applications-admits-rejects/university-of-connecticut/2?page=5"
-token,session = S_ex.getAuthSession()
-soup = U_ex.open_url_2_soup(url, session_request = session)
